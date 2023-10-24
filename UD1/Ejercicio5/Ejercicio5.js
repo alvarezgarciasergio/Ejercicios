e305d75c511f7c4f8ee592f821cd1a72ab4b9c26
alvarezgarciasergio <alvarez.garcia.sergio@gmail.com>
2023-10-24 10:31:51 +0200

    Enunciado ejer 5

--- UD1/Ejercicio5/Ejercicio5.js
+++ UD1/Ejercicio5/Ejercicio5.js
@@ -1,3 +1,13 @@
+/*
+Prepara un programa que construya una matriz de veinte números aleatorios entre 0 y 10. Una vez
+construida dicha matriz, el programa debe calcular cual es la moda.
+Para obtener un número aleatorio entre 0 y 10 puedes utilizar el siguiente código:
+Math.floor(Math.random() * 10);
+Un ejemplo de ejecución del programa sería:
+[ 1, 2, 4, 5, 4 ,4 ,4 ,4 ,4 ,4 ,0 ,7, 8, 4 ,9, 7, 3, 3, 1, 0]
+Moda: 4
+*/
+
 let matriz=[]
 
 for (let i=0;i<20;i++){
